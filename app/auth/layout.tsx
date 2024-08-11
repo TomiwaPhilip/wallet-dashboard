72820049aeb9cdda207c8cb412c40acfbe7b184f
Tomiwa Philip <110630370+TomiwaPhilip@users.noreply.github.com>
2024-08-11 10:22:00 +0000

    added ga to auth

--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "../globals.css";
 import { Card } from "@/components/shared/shared";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 export const metadata: Metadata = {
   title: "Auth",
@@ -18,6 +19,7 @@ export default function RootLayout({
         <main className="h-screen flex items-center justify-center">
           <Card>{children}</Card>
         </main>
+        <GoogleAnalytics gaId="G-4L4BM0EEW7" />
       </body>
     </html>
   );
